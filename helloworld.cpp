aff00ab9367e886f173ac5d9b211cbbf9eca818f
Joan Kathleen Aviles <contactjkeaviles@gmail.com>
2024-09-24 17:32:56 +0800

    Update helloworld.cpp
    fixed the quotation marks to show when I run the program

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,17 +1,18 @@
-using namespace std;
 #include <iostream>
-#include <stdio.h>
+using namespace std;
 
 int main ()
+
 {
   string name = "joan the explorer";
   string pet = "kuki the car";
   int age = 17;
   int number = 44;
-    cout << "\"Hello World"\" << endl ;
-    cout << "\"I am Name " << name << "\" << endl ;
-    cout << "\"My cat's name is " << pet << "\" << endl ;
-    cout << "\"I am now " << age << " years old"\" << endl ;
-    cout << "My battery percentage " << number << "%" "\" << endl ;
+    cout << "\"Hello World\"" << endl;
+    cout << "\"I am name " << name << "\"" << endl;
+    cout << "\"My cat's name is " << pet << "\"" << endl;
+    cout << "\"I am now " << age << " years old\"" << endl;
+    cout << "\"My battery percentage " << number << "%" "\"" << endl;
+    
   return 0;
 }
